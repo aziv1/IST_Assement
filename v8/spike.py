f0c4eedba3f53fb2f943d5b57ed7361f9777bf06
aziv1 <114120102+aziv1@users.noreply.github.com>
2024-08-29 19:51:42 +1000

    Update spike.py

--- v8/spike.py
+++ v8/spike.py
@@ -1,19 +1,21 @@
 from hub import port
-import color_sensor
-import color
+from motor import absolute_position
+import motor
 import runloop
 import motor_pair
-
+import color_sensor
+import color
 async def main():
-    motor_pair.pair(motor_pair.PAIR_1, port.A, port.C)
-    while True:
+     motor_pair.pair(motor_pair.PAIR_1, port.A, port.C)
+     while True:
         if color_sensor.color(port.E) is color.RED:
-            print("Seen Red")
-            await motor_pair.move_for_degrees(motor_pair.PAIR_1, 160, 0)
+            print("seen red")
+            await motor_pair.move_for_degrees(motor_pair.PAIR_1, 270, 0)
+            await motor_pair.move_for_degrees(motor_pair.PAIR_1, -270, 0)
         elif color_sensor.color(port.E) is color.BLUE:
-            print("Seen Blue")
-            await motor_pair.move_for_degrees(motor_pair.PAIR_1, -160, 0)
+            print("seen blue")
+            await motor_pair.move_for_degrees(motor_pair.PAIR_1, -270, 0)
+            await motor_pair.move_for_degrees(motor_pair.PAIR_1, 270, 0)
         else:
             motor_pair.stop(motor_pair.PAIR_1)
-
 runloop.run(main())
